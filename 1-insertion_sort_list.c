defe0d65b51ec05ae5faf9312314eccdb42adec3
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-04 13:54:37 +0200

    Commit

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,50 +1,44 @@
 #include "sort.h"
+
 /**
- * insertion_sort_list - Insertion Sort for list
+ * insertion_sort_list - insertion sort list algorthms
  * @list: list pointer
- * Return: Nothing
+ * Return: No Return
  */
 void insertion_sort_list(listint_t **list)
 {
-        listint_t *sorted, *curr, *next, *temp, *aux;
+	listint_t *curr, *swap, *tmp;
 
-        sorted = NULL;
-        curr = *list;
-
-        while (curr != NULL)
-        {
-                next = curr->next;
-
-                if (sorted == NULL || sorted->n > curr->n)
-                {
-                        curr->next = sorted;
-                        sorted = curr;
-                }
-                else
-                {
-                        aux = sorted;
-
-                        while (aux->next != NULL && aux->next->n <= curr->n)
-                        {
-                                aux = aux->next;
-                        }
-                        if (aux->next == NULL)
-                        {
-                                aux->next = curr;
-                                curr->prev = aux;
-                                curr->next = NULL;
-                                print_list(*curr);
-                        }
-                        else
-                        {
-                                temp = aux->next;
-                                aux->next = curr;
-                                curr->prev = aux;
-                                curr->next = temp;
-                                temp->prev = curr;
-                        }
-                }
-                curr = next;
-        }
-        *list = sorted;
+	if (!list || *list == NULL)
+		return;
+	curr = *list;
+	/* validate if there is only one element in list */
+	if (curr->next == NULL)
+		return;
+	while (curr->next != NULL)
+	{
+		swap = curr->next;
+		/* Comparison starts here */
+		if (curr->n > swap->n)
+		{
+			tmp = curr;
+			while (tmp != NULL && tmp->n > swap->n)
+			{
+				tmp->next = swap->next;
+				if (tmp->next != NULL)
+					tmp->next->prev = tmp;
+				swap->prev = tmp->prev;
+				if (swap->prev != NULL)
+					swap->prev->next = swap;
+				else
+					*list = swap;
+				tmp->prev = swap;
+				swap->next = tmp;
+				print_list(*list);
+				tmp = swap->prev;
+			}
+			continue;
+		}
+		curr = curr->next;
+	}
 }
