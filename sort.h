87e2affa94fcb6636d8f22a8079189900fb548a3
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-04 10:06:07 +0200

    Commit

--- sort.h
+++ sort.h
@@ -21,7 +21,7 @@ typedef struct listint_s
 
 /** Function Prototype **/
 void print_array(const  int *array, size_t size);
-void print_list(const list
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 
 #endif
