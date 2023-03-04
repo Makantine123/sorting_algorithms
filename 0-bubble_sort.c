61ad305c0af4c867160878087475e19cff7ecd44
Siphelele makhathini <siphelele.mlungisi@gail.com>
2023-03-04 11:29:38 +0200

    Commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,19 +7,19 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    unsigned int i, j, swap;
+unsigned int i, j, swap;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size -i - 1; j++)
-        {
-            if ( array[j] > array[j + 1])
-            {
-                swap = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = swap;
-                print_array(array, size);
-            }
-        }
-    }
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{
+if (array[j] > array[j + 1])
+{
+swap = array[j];
+array[j] = array[j + 1];
+array[j + 1] = swap;
+print_array(array, size);
+}
+}
+}
 }
