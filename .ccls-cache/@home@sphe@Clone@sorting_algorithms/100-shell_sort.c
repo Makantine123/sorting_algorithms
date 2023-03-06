71f7587e147ed976a7bfc50f09f84158961fac2e
Siphelele Makhathini <siphelele.mlungisi@gmail.com>
2023-03-06 13:59:50 +0200

    Commit

--- .ccls-cache/@home@sphe@Clone@sorting_algorithms/100-shell_sort.c
+++ .ccls-cache/@home@sphe@Clone@sorting_algorithms/100-shell_sort.c
@@ -8,5 +8,19 @@
  */
 void shell_sort(int *array, size_t size)
 {
+	int interval, i, j, temp;
 
+	for (interval = size / 2; interval > 0; interval /= 2)
+	{
+		for (i = interval; i < size; i += 1)
+		{
+			temp = array[i];
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			{
+				array[j] = array[j - interval];
+			}
+			array[j] = temp;
+			print_array(array, size);
+		}
+	}
 }
